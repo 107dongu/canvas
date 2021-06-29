12bf282d76854a260dc3dc7964667e09671955c9
dokang <107dongu@gmail.com>
2021-06-29 14:52:00 +0900

    add js file and add mouse event for draw line

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+
+// pixcel modifier에 사이즈 지정
+canvas.width = 700;
+canvas.height = 700;
+
+// context 안에 있는 색과 크기 지정
+ctx.strokeStyle = "##2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,13 +16,24 @@ function stopPainting() {
 	painting = false;	
 }
 
+function startPainting() {
+	painting = true;
+}
+
 function onMouseMove(event) {
 	const x = event.offsetX;
 	const y = event.offsetY;
+	if (!painting) {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+	} else {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
 }
 
 function onMouseDown(event) {
-	painting = true;
+	startPainting();
 }
 
 function onMouseUp(event) {
